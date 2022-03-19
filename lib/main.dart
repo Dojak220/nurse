6e81f73f29e01a6177361f10a62a25615e2f312e
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-03-19 20:35:50 -0300

    start database before run app

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:nurse/app/pages/Home/home.dart';
+import 'package:nurse/app/repositories/database/database_manager.dart';
 import 'package:nurse/app/theme/app_theme.dart';
 
 void main() async {
-  loadEnviromentVariables().then((_) {
-    runApp(Nurse());
+  loadEnviromentVariables().whenComplete(() {
+    startDatabase().whenComplete(() {
+      runApp(Nurse());
+    });
   });
 }
 
 Future<void> loadEnviromentVariables() async {
   await dotenv.load();
+}
+
+Future<void> startDatabase() async {
+  await DatabaseManager().tryToInit();
 }
 
 class Nurse extends StatelessWidget {
