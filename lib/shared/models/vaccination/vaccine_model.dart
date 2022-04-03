82f973cb4c216b6d893bf685a2523bdf9b08c890
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-04-03 15:32:23 -0300

    Add tests to vaccine model

--- lib/shared/models/vaccination/vaccine_model.dart
+++ lib/shared/models/vaccination/vaccine_model.dart
@@ -1,34 +1,55 @@
+import 'package:nurse/shared/models/generic_model.dart';
 import 'package:nurse/shared/models/vaccination/vaccine_batch_model.dart';
+import 'package:nurse/shared/utils/validator.dart';
 
-class Vaccine {
+class Vaccine implements GenericModel {
+  @override
+  final int id;
   final String sipniCode;
   final String name;
   final String laboratory;
   final VaccineBatch vaccineBatch;
 
-  Vaccine(
-    this.sipniCode,
-    this.name,
-    this.laboratory,
-    this.vaccineBatch,
-  );
+  Vaccine({
+    required this.id,
+    required String sipniCode,
+    required String name,
+    required String laboratory,
+    required this.vaccineBatch,
+  })  : this.sipniCode = sipniCode.trim(),
+        this.name = name.trim(),
+        this.laboratory = laboratory.trim() {
+    _validateVaccine();
+  }
+
+  void _validateVaccine() {
+    Validator.validateAll([
+      ValidationPair(ValidatorType.Id, this.id),
+      ValidationPair(ValidatorType.NumericalString, this.sipniCode),
+      ValidationPair(ValidatorType.Name, this.name),
+      ValidationPair(ValidatorType.Name, this.laboratory),
+    ]);
+  }
 
   Vaccine copyWith({
+    int? id,
     String? sipniCode,
     String? name,
     String? laboratory,
     VaccineBatch? vaccineBatch,
   }) {
     return Vaccine(
-      sipniCode ?? this.sipniCode,
-      name ?? this.name,
-      laboratory ?? this.laboratory,
-      vaccineBatch ?? this.vaccineBatch,
+      id: id ?? this.id,
+      sipniCode: sipniCode ?? this.sipniCode,
+      name: name ?? this.name,
+      laboratory: laboratory ?? this.laboratory,
+      vaccineBatch: vaccineBatch ?? this.vaccineBatch,
     );
   }
 
-  Map<String, dynamic> toMap() {
+  Map<String, Object> toMap() {
     return {
+      'id': id,
       'sipniCode': sipniCode,
       'name': name,
       'laboratory': laboratory,
@@ -38,16 +59,17 @@ class Vaccine {
 
   factory Vaccine.fromMap(Map<String, dynamic> map) {
     return Vaccine(
-      map['sipniCode'] ?? '',
-      map['name'] ?? '',
-      map['laboratory'] ?? '',
-      VaccineBatch.fromMap(map['vaccineBatch']),
+      id: map['id']?.toInt() ?? 0,
+      sipniCode: map['sipniCode'] ?? '',
+      name: map['name'] ?? '',
+      laboratory: map['laboratory'] ?? '',
+      vaccineBatch: VaccineBatch.fromMap(map['vaccineBatch']),
     );
   }
 
   @override
   String toString() {
-    return 'Vaccine(sipniCode: $sipniCode, name: $name, laboratory: $laboratory, vaccineBatch: $vaccineBatch)';
+    return 'Vaccine(id: $id, sipniCode: $sipniCode, name: $name, laboratory: $laboratory, vaccineBatch: $vaccineBatch)';
   }
 
   @override
@@ -55,6 +77,7 @@ class Vaccine {
     if (identical(this, other)) return true;
 
     return other is Vaccine &&
+        other.id == id &&
         other.sipniCode == sipniCode &&
         other.name == name &&
         other.laboratory == laboratory &&
@@ -63,7 +86,8 @@ class Vaccine {
 
   @override
   int get hashCode {
-    return sipniCode.hashCode ^
+    return id.hashCode ^
+        sipniCode.hashCode ^
         name.hashCode ^
         laboratory.hashCode ^
         vaccineBatch.hashCode;
