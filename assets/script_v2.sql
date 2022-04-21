564ad0024cae6adeba7c3f21e4203e6afd5ffa3c
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-04-20 21:49:28 -0300

    Remove AUTOINCREMENT key and eof comment

--- assets/script_v2.sql
+++ assets/script_v2.sql
@@ -4,7 +4,7 @@
 -- tables
 -- Table: Aplicacao
 CREATE TABLE Aplicacao (
-    id integer NOT NULL CONSTRAINT Aplicacao_pk PRIMARY KEY AUTOINCREMENT,
+    id integer NOT NULL CONSTRAINT Aplicacao_pk PRIMARY KEY,
     aplicante_id integer NOT NULL,
     vacina_id integer NOT NULL,
     dose varchar(4) NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE Aplicacao (
 
 -- Table: Aplicante
 CREATE TABLE Aplicante (
-    id integer NOT NULL CONSTRAINT Aplicante_pk PRIMARY KEY AUTOINCREMENT,
+    id integer NOT NULL CONSTRAINT Aplicante_pk PRIMARY KEY,
     cns varchar(15) NOT NULL,
     nome varchar(50) NOT NULL,
     estabelecimento_id integer NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE Aplicante (
 
 -- Table: Campanha
 CREATE TABLE Campanha (
-    titulo varchar(30) NOT NULL AUTOINCREMENT,
+    titulo varchar(30) NOT NULL,
     inicio date NOT NULL,
     termino date NOT NULL,
     descricao varchar(100) NOT NULL,
@@ -48,7 +48,7 @@ CREATE TABLE Campanha (
 
 -- Table: Categoria_Prioritaria
 CREATE TABLE Categoria_Prioritaria (
-    id integer NOT NULL CONSTRAINT Grupo_pk PRIMARY KEY AUTOINCREMENT,
+    id integer NOT NULL CONSTRAINT Grupo_pk PRIMARY KEY,
     codigo varchar(10) NOT NULL,
     nome varchar(10) NOT NULL,
     grupo_id integer NOT NULL,
@@ -60,7 +60,7 @@ CREATE TABLE Categoria_Prioritaria (
 
 -- Table: Estabelecimento
 CREATE TABLE Estabelecimento (
-    id integer NOT NULL CONSTRAINT Estabelecimento_pk PRIMARY KEY AUTOINCREMENT,
+    id integer NOT NULL CONSTRAINT Estabelecimento_pk PRIMARY KEY,
     cnes varchar(20) NOT NULL,
     nome varchar(50) NOT NULL,
     localidade_id integer NOT NULL,
@@ -71,7 +71,7 @@ CREATE TABLE Estabelecimento (
 
 -- Table: Grupo_Prioritario
 CREATE TABLE Grupo_Prioritario (
-    id integer NOT NULL CONSTRAINT Grupo_pk PRIMARY KEY AUTOINCREMENT,
+    id integer NOT NULL CONSTRAINT Grupo_pk PRIMARY KEY,
     codigo varchar(10) NOT NULL,
     nome varchar(10) NOT NULL,
     descricao varchar(150) NOT NULL,
@@ -80,7 +80,7 @@ CREATE TABLE Grupo_Prioritario (
 
 -- Table: Localidade
 CREATE TABLE Localidade (
-    id integer NOT NULL CONSTRAINT Localidade_pk PRIMARY KEY AUTOINCREMENT,
+    id integer NOT NULL CONSTRAINT Localidade_pk PRIMARY KEY,
     nome varchar(40) NOT NULL,
     municipio varchar(40) NOT NULL,
     estado varchar(2) NOT NULL,
@@ -90,7 +90,7 @@ CREATE TABLE Localidade (
 
 -- Table: Lote
 CREATE TABLE Lote (
-    id integer NOT NULL CONSTRAINT Vacina_pk PRIMARY KEY AUTOINCREMENT,
+    id integer NOT NULL CONSTRAINT Vacina_pk PRIMARY KEY,
     numero_lote varchar(10) NOT NULL,
     quantidade integer NOT NULL,
     CONSTRAINT numero_lote UNIQUE (numero_lote)
@@ -98,7 +98,7 @@ CREATE TABLE Lote (
 
 -- Table: Paciente
 CREATE TABLE Paciente (
-    id integer NOT NULL CONSTRAINT Paciente_pk PRIMARY KEY AUTOINCREMENT,
+    id integer NOT NULL CONSTRAINT Paciente_pk PRIMARY KEY,
     cns varchar(15) NOT NULL,
     condicao_maternal varchar(10) NOT NULL,
     pessoa_id integer NOT NULL,
@@ -113,7 +113,7 @@ CREATE TABLE Paciente (
 
 -- Table: Pessoa
 CREATE TABLE Pessoa (
-    id integer NOT NULL CONSTRAINT Paciente_pk PRIMARY KEY AUTOINCREMENT,
+    id integer NOT NULL CONSTRAINT Paciente_pk PRIMARY KEY,
     cpf varchar(11) NOT NULL,
     nome varchar(50) NOT NULL,
     nascimento date NOT NULL,
@@ -128,7 +128,7 @@ CREATE TABLE Pessoa (
 
 -- Table: Vacina
 CREATE TABLE Vacina (
-    id integer NOT NULL CONSTRAINT Vacina_pk PRIMARY KEY AUTOINCREMENT,
+    id integer NOT NULL CONSTRAINT Vacina_pk PRIMARY KEY,
     codigo_sipni varchar(10) NOT NULL,
     nome varchar(15) NOT NULL,
     laboratorio varchar(30) NOT NULL,
@@ -137,6 +137,3 @@ CREATE TABLE Vacina (
     CONSTRAINT Lote_Vacina FOREIGN KEY (lote_id)
     REFERENCES Lote (id)
 );
-
--- End of file.
-
