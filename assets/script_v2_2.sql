4019ba416148ac57015898dc18296c3ba5e630ca
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-05-05 11:08:21 -0300

    Change script to english

--- assets/script_v2_2.sql
+++ assets/script_v2_2.sql
@@ -3,171 +3,173 @@
 
 -- tables
 -- Table: Aplicacao
-CREATE TABLE Aplicacao (
-    id integer NOT NULL CONSTRAINT Aplicacao_pk PRIMARY KEY,
-    aplicante_id integer NOT NULL,
-    vacina_id integer NOT NULL,
+CREATE TABLE Application (
+    id integer NOT NULL CONSTRAINT Application_pk PRIMARY KEY,
+    applier integer NOT NULL,
+    vaccine integer NOT NULL,
     dose varchar(4) NOT NULL,
-    paciente_id integer NOT NULL,
-    data date NOT NULL,
-    campanha_id integer,
-    data_aprazamento date,
-    CONSTRAINT Paciente_Aplicacao FOREIGN KEY (paciente_id)
-    REFERENCES Paciente (id),
-    CONSTRAINT Aplicacao_Aplicante FOREIGN KEY (aplicante_id)
-    REFERENCES Aplicante (id),
-    CONSTRAINT Aplicacao_Campanha FOREIGN KEY (campanha_id)
-    REFERENCES Campanha (id),
-    CONSTRAINT Aplicacao_Vacina FOREIGN KEY (vacina_id)
-    REFERENCES Vacina (id)
+    patient integer NOT NULL,
+    application_date date NOT NULL,
+    campaign integer,
+    due_date date,
+    CONSTRAINT Patient_Application FOREIGN KEY (patient)
+    REFERENCES Patient (id),
+    CONSTRAINT Application_Applier FOREIGN KEY (applier)
+    REFERENCES Applier (id),
+    CONSTRAINT Application_Campaign FOREIGN KEY (campaign)
+    REFERENCES Campaign (id),
+    CONSTRAINT Application_Vaccine FOREIGN KEY (vaccine)
+    REFERENCES Vaccine (id)
 );
 
 -- Table: Aplicante
-CREATE TABLE Aplicante (
-    id integer NOT NULL CONSTRAINT Aplicante_pk PRIMARY KEY,
+CREATE TABLE Applier (
+    id integer NOT NULL CONSTRAINT Applier_pk PRIMARY KEY,
     cns varchar(15) NOT NULL,
-    nome varchar(50) NOT NULL,
-    estabelecimento_id integer NOT NULL,
-    pessoa_id integer NOT NULL,
-    CONSTRAINT cns_aplicante UNIQUE (cns),
-    CONSTRAINT pessoa_id_aplicante UNIQUE (pessoa_id),
-    CONSTRAINT Aplicante_Estabelecimento FOREIGN KEY (estabelecimento_id)
-    REFERENCES Estabelecimento (id),
-    CONSTRAINT Aplicante_Pessoa FOREIGN KEY (pessoa_id)
-    REFERENCES Pessoa (id)
+    establishment integer NOT NULL,
+    person integer NOT NULL,
+    CONSTRAINT cns_applier UNIQUE (cns),
+    CONSTRAINT person_applier UNIQUE (person),
+    CONSTRAINT Applier_Establishment FOREIGN KEY (establishment)
+    REFERENCES Establishment (id),
+    CONSTRAINT Applier_Person FOREIGN KEY (person)
+    REFERENCES Person (id)
 );
 
 -- Table: Campanha
-CREATE TABLE Campanha (
-    titulo varchar(30) NOT NULL,
-    inicio date NOT NULL,
-    termino date NOT NULL,
-    descricao varchar(100) NOT NULL,
-    id integer NOT NULL CONSTRAINT Campanha_pk PRIMARY KEY
+CREATE TABLE Campaign (
+    title varchar(30) NOT NULL,
+    start_date date NOT NULL,
+    end_date date NOT NULL,
+    description varchar(100) NOT NULL,
+    id integer NOT NULL CONSTRAINT Campaign_pk PRIMARY KEY
 );
 
 -- Table: Categoria_Prioritaria
-CREATE TABLE Categoria_Prioritaria (
-    id integer NOT NULL CONSTRAINT Grupo_pk PRIMARY KEY,
-    codigo varchar(10) NOT NULL,
-    nome varchar(10) NOT NULL,
-    grupo_id integer NOT NULL,
-    descricao varchar(150) NOT NULL,
-    CONSTRAINT codigo_categoria UNIQUE (codigo),
-    CONSTRAINT Categoria_Grupo_Prioritario FOREIGN KEY (grupo_id)
-    REFERENCES Grupo_Prioritario (id)
+CREATE TABLE Priority_Category (
+    id integer NOT NULL CONSTRAINT Group_pk PRIMARY KEY,
+    code varchar(10) NOT NULL,
+    name varchar(10) NOT NULL,
+    "group" integer NOT NULL,
+    description varchar(150) NOT NULL,
+    CONSTRAINT code_category UNIQUE (code),
+    CONSTRAINT Category_Priority_Group FOREIGN KEY ("group")
+    REFERENCES Priority_Group (id)
 );
 
 -- Table: Estabelecimento
-CREATE TABLE Estabelecimento (
-    id integer NOT NULL CONSTRAINT Estabelecimento_pk PRIMARY KEY,
+CREATE TABLE Establishment (
+    id integer NOT NULL CONSTRAINT Establishment_pk PRIMARY KEY,
     cnes varchar(7) NOT NULL,
-    nome varchar(50) NOT NULL,
-    localidade_id integer NOT NULL,
+    name varchar(50) NOT NULL,
+    locality integer NOT NULL,
     CONSTRAINT cnes UNIQUE (cnes),
-    CONSTRAINT Estabelecimento_Localidade FOREIGN KEY (localidade_id)
-    REFERENCES Localidade (id)
+    CONSTRAINT Establishment_Locality FOREIGN KEY (locality)
+    REFERENCES Locality (id)
 );
 
 -- Table: Grupo_Prioritario
-CREATE TABLE Grupo_Prioritario (
-    id integer NOT NULL CONSTRAINT Grupo_pk PRIMARY KEY,
-    codigo varchar(10) NOT NULL,
-    nome varchar(10) NOT NULL,
-    descricao varchar(150) NOT NULL,
-    CONSTRAINT codigo_grupo UNIQUE (codigo)
+CREATE TABLE Priority_Group (
+    id integer NOT NULL CONSTRAINT Group_pk PRIMARY KEY,
+    code varchar(10) NOT NULL,
+    name varchar(10) NOT NULL,
+    description varchar(150) NOT NULL,
+    CONSTRAINT code_group UNIQUE (code)
 );
 
 -- Table: Localidade
-CREATE TABLE Localidade (
-    id integer NOT NULL CONSTRAINT Localidade_pk PRIMARY KEY,
-    nome varchar(40) NOT NULL,
-    municipio varchar(40) NOT NULL,
-    estado varchar(2) NOT NULL,
-    codigo_ibge varchar(7) NOT NULL,
-    CONSTRAINT codigo_ibge UNIQUE (codigo_ibge)
+CREATE TABLE Locality (
+    id integer NOT NULL CONSTRAINT Locality_pk PRIMARY KEY,
+    name varchar(40) NOT NULL,
+    city varchar(40) NOT NULL,
+    state varchar(2) NOT NULL,
+    ibge_code varchar(7) NOT NULL,
+    CONSTRAINT ibge_code UNIQUE (ibge_code)
 );
 
 -- Table: Lote
-CREATE TABLE Lote (
-    id integer NOT NULL CONSTRAINT Vacina_pk PRIMARY KEY,
-    numero_lote varchar(10) NOT NULL,
-    quantidade integer NOT NULL,
-    CONSTRAINT numero_lote UNIQUE (numero_lote)
+CREATE TABLE Vaccine_Batch (
+    id integer NOT NULL CONSTRAINT Vaccine_pk PRIMARY KEY,
+    number varchar(10) NOT NULL,
+    quantity integer NOT NULL,
+    CONSTRAINT number UNIQUE (number)
 );
 
 -- Table: Paciente
-CREATE TABLE Paciente (
-    id integer NOT NULL CONSTRAINT Paciente_pk PRIMARY KEY,
+CREATE TABLE Patient (
+    id integer NOT NULL CONSTRAINT Patient_pk PRIMARY KEY,
     cns varchar(15) NOT NULL,
-    condicao_maternal varchar(10) NOT NULL,
-    pessoa_id integer NOT NULL,
-    categoria_id integer NOT NULL,
-    CONSTRAINT cns_paciente UNIQUE (cns),
-    CONSTRAINT pessoa_id_paciente UNIQUE (pessoa_id),
-    CONSTRAINT Paciente_Pessoa FOREIGN KEY (pessoa_id)
-    REFERENCES Pessoa (id),
-    CONSTRAINT Paciente_Categoria FOREIGN KEY (categoria_id)
-    REFERENCES Categoria_Prioritaria (id)
+    maternal_condition varchar(10) NOT NULL,
+    person integer NOT NULL,
+    category integer NOT NULL,
+    CONSTRAINT cns_patient UNIQUE (cns),
+    CONSTRAINT person_patient UNIQUE (person),
+    CONSTRAINT Patient_Person FOREIGN KEY (person)
+    REFERENCES Person (id),
+    CONSTRAINT Patient_Category FOREIGN KEY (category)
+    REFERENCES Priority_Category (id)
 );
 
 -- Table: Pessoa
-CREATE TABLE Pessoa (
-    id integer NOT NULL CONSTRAINT Paciente_pk PRIMARY KEY,
+CREATE TABLE Person (
+    id integer NOT NULL CONSTRAINT Person_pk PRIMARY KEY,
     cpf varchar(11) NOT NULL,
-    nome varchar(50) NOT NULL,
-    nascimento date NOT NULL,
-    sexo varchar(1) NOT NULL,
-    nome_mae varchar(50) NOT NULL,
-    nome_pai varchar(50) NOT NULL,
-    localidade_id integer NOT NULL,
+    name varchar(50) NOT NULL,
+    birth_date date NOT NULL,
+    sex varchar(1) NOT NULL,
+    mother_name varchar(50) NOT NULL,
+    father_name varchar(50) NOT NULL,
+    locality integer NOT NULL,
     CONSTRAINT cpf UNIQUE (cpf),
-    CONSTRAINT Pessoa_Localidade FOREIGN KEY (localidade_id)
-    REFERENCES Localidade (id)
+    CONSTRAINT Person_Locality FOREIGN KEY (locality)
+    REFERENCES Locality (id)
 );
 
 -- Table: Vacina
-CREATE TABLE Vacina (
-    id integer NOT NULL CONSTRAINT Vacina_pk PRIMARY KEY,
-    codigo_sipni varchar(10) NOT NULL,
-    nome varchar(15) NOT NULL,
-    laboratorio varchar(30) NOT NULL,
-    lote_id integer NOT NULL,
-    CONSTRAINT codigo_sipni UNIQUE (codigo_sipni),
-    CONSTRAINT Lote_Vacina FOREIGN KEY (lote_id)
-    REFERENCES Lote (id)
+CREATE TABLE Vaccine (
+    id integer NOT NULL CONSTRAINT Vaccine_pk PRIMARY KEY,
+    sipni_code varchar(10) NOT NULL,
+    name varchar(15) NOT NULL,
+    laboratory varchar(30) NOT NULL,
+    batch integer NOT NULL,
+    CONSTRAINT sipni_code UNIQUE (sipni_code),
+    CONSTRAINT Vaccine_Batch FOREIGN KEY (batch)
+    REFERENCES Vaccine_Batch (id)
 );
 
 
-INSERT INTO Aplicacao (aplicante_id, vacina_id, dose, paciente_id , data, campanha_id , data_aprazamento)
+INSERT INTO Application (applier, vaccine, dose, patient , application_date, campaign , due_date)
 VALUES (1, 1, "D1", 1, "2021-01-01", 1, "2021-04-01");
 
-INSERT INTO Aplicante (cns, nome, estabelecimento_id, pessoa_id)
-VALUES ('278794316530006', 'Maria Mercedes Motta', 1, 1);
+INSERT INTO Applier (cns, establishment, person)
+VALUES ('278794316530006', 1, 1);
 
-INSERT INTO Campanha (titulo, inicio, termino, descricao)
+INSERT INTO Campaign (title, start_date, end_date, description)
 VALUES ('Campanha de Vacinação', '2020-01-01', '2020-12-31', 'Campanha de Vacinação');
 
-INSERT INTO Categoria_Prioritaria (codigo, nome, grupo_id, descricao)
+INSERT INTO Priority_Category (code, name, "group", description)
 VALUES ('Idosos', 'Idosos', 1, 'Categoria de pessoas com mais de 60 anos');
 
-INSERT INTO Estabelecimento (cnes, nome, localidade_id)
+INSERT INTO Establishment (cnes, name, locality)
 VALUES ('1234567', 'Estabelecimento 1', 1);
 
-INSERT INTO Grupo_Prioritario (codigo, nome, descricao)
+INSERT INTO Priority_Group (code, name, description)
 VALUES ('Idosos', 'Idosos', 'Grupo de pessoas com mais de 60 anos');
 
-INSERT INTO Localidade (nome, municipio, estado, codigo_ibge)
+INSERT INTO Locality (name, city, state, ibge_code)
 VALUES ('São Paulo', 'São Paulo', 'SP', '3550308');
 
-INSERT INTO Lote (numero_lote, quantidade)
+INSERT INTO Vaccine_Batch (number, quantity)
 VALUES ('123456', 10);
 
-INSERT INTO Paciente (cns, condicao_maternal, pessoa_id, categoria_id)
-VALUES ('832070136190005', 'NENHUMA', 1, 1);
+INSERT INTO Patient (cns, maternal_condition, person, category)
+VALUES ('832070136190005', 'NENHUMA', 2, 1);
 
-INSERT INTO Pessoa (cpf, nome, nascimento, sexo, nome_mae, nome_pai, localidade_id)
+INSERT INTO Person (cpf, name, birth_date, sex, mother_name, father_name, locality)
 VALUES ('69309106271', 'Maria', '2000-01-01', 'F', 'Maria Madalena', 'João Pedro', 1);
 
-INSERT INTO Vacina (codigo_sipni, nome, laboratorio, lote_id)
+INSERT INTO Person (cpf, name, birth_date, sex, mother_name, father_name, locality)
+VALUES ('63773671040', 'Marcia', '2000-03-01', 'F', 'Maria Madalena', 'João Pedro', 1);
+
+INSERT INTO Vaccine (sipni_code, name, laboratory, batch)
 VALUES ('VAC-01', 'Vacina 1', 'Laboratorio 1', 1);
