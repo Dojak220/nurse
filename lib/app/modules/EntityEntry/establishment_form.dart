ae6a0fa001a19cdef685d9e0812b027ef8649d18
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-08-21 15:03:43 -0300

    Finish establishment form page

--- lib/app/modules/EntityEntry/establishment_form.dart
+++ lib/app/modules/EntityEntry/establishment_form.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:nurse/app/modules/EntityEntry/establishment_form_controller.dart';
+import 'package:nurse/app/modules/VaccinationEntry/components/custom_dropdown_button_form_field%20.dart';
 import 'package:nurse/app/modules/VaccinationEntry/components/custom_form_field.dart';
 import 'package:nurse/app/modules/VaccinationEntry/components/custom_text_form_field.dart';
+import 'package:nurse/app/modules/VaccinationEntry/components/dialog_confirm_button.dart';
+import 'package:nurse/app/theme/app_theme.dart';
+import 'package:nurse/shared/models/infra/locality_model.dart';
 import 'package:nurse/shared/utils/validator.dart';
 
 class EstablishmentForm extends StatefulWidget {
@@ -14,10 +18,39 @@ class EstablishmentForm extends StatefulWidget {
 }
 
 class _EstablishmentFormState extends State<EstablishmentForm> {
+  List<Locality> _localityCities = List<Locality>.empty(growable: true);
+
+  @override
+  void initState() {
+    _getCities();
+    super.initState();
+  }
+
+  Future<void> _getCities() async {
+    _localityCities = await widget.controller.getCitiesFromLocalities();
+    setState(() {});
+  }
+
+  void tryToSave(
+    EstablishmentFormController controller,
+  ) async {
+    final wasSaved = await controller.saveInfo();
+
+    if (wasSaved) {
+      Navigator.of(context).pop();
+    } else {
+      showDialog(
+        context: context,
+        builder: (_) {
+          return RegistrationFailedAlertDialog(entityName: "estabelecimento");
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as Map;
-
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -28,32 +61,122 @@ class _EstablishmentFormState extends State<EstablishmentForm> {
         ),
       ),
       body: SafeArea(
-        child: Form(
-          key: widget.controller.formKey,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4.0),
-            child: ListView(
-              children: [
-                CustomTextFormField(
-                  icon: Icon(Icons.local_hospital),
-                  label: FormLabels.establishmentCNES,
-                  textEditingController: widget.controller.cnes,
-                  validatorType: ValidatorType.CNES,
-                  onSaved: (value) => {},
-                ),
-                Divider(color: Colors.black),
-                CustomTextFormField(
-                  icon: Icon(Icons.abc),
-                  label: FormLabels.establishmentName,
-                  textEditingController: widget.controller.name,
-                  validatorType: ValidatorType.Name,
-                  onSaved: (value) => {},
-                ),
-              ],
+        child: Column(
+          children: [
+            Expanded(
+              child: _EstablishmentFormFields(
+                controller: widget.controller,
+                localityCities: _localityCities,
+              ),
             ),
-          ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: const EdgeInsets.all(5),
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: AppTheme.stepButtonStyle,
+                  onPressed: () => tryToSave(widget.controller),
+                  child: Text("Salvar"),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class _EstablishmentFormFields extends StatefulWidget {
+  final EstablishmentFormController controller;
+  final List<Locality> localityCities;
+
+  const _EstablishmentFormFields(
+      {Key? key, required this.controller, required this.localityCities})
+      : super(key: key);
+
+  @override
+  State<_EstablishmentFormFields> createState() =>
+      _EstablishmentFormFieldsState();
+}
+
+class _EstablishmentFormFieldsState extends State<_EstablishmentFormFields> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: widget.controller.formKey,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+        child: ListView(
+          shrinkWrap: true,
+          semanticChildCount: 3,
+          children: [
+            CustomDropdownButtonFormField(
+              icon: Icon(Icons.pin),
+              label: FormLabels.establishmentLocalityName,
+              items: widget.localityCities
+                  .map<String>((Locality l) => l.city)
+                  .toList(),
+              value: widget.controller.locality?.city,
+              onChanged: (String? city) => setState(
+                () => widget.controller.locality =
+                    widget.localityCities.singleWhere((lc) => lc.city == city),
+              ),
+              onSaved: (String? city) => widget.controller.locality =
+                  widget.localityCities.singleWhere((lc) => lc.city == city),
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              icon: Icon(Icons.local_hospital),
+              label: FormLabels.establishmentCNES,
+              textEditingController: widget.controller.cnes,
+              validatorType: ValidatorType.CNES,
+              onSaved: (value) => {},
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              icon: Icon(Icons.abc),
+              label: FormLabels.establishmentName,
+              textEditingController: widget.controller.name,
+              validatorType: ValidatorType.Name,
+              onSaved: (value) => {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RegistrationFailedAlertDialog extends StatelessWidget {
+  final String entityName;
+
+  const RegistrationFailedAlertDialog({
+    Key? key,
+    required this.entityName,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const Icon(Icons.warning, size: 120.0),
+          Text(
+            'Falha ao cadastrar novo(a) $entityName!',
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+      content: Text(
+        'Cadastro já existia no banco de dados!',
+        textAlign: TextAlign.center,
+      ),
+      actions: [DialogConfirmButton(text: "Ok")],
+      actionsAlignment: MainAxisAlignment.spaceAround,
     );
   }
 }
--- lib/app/modules/EntityEntry/establishment_form_controller.dart
+++ lib/app/modules/EntityEntry/establishment_form_controller.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:nurse/app/utils/form_controller.dart';
+import 'package:nurse/shared/models/infra/establishment_model.dart';
 import 'package:nurse/shared/models/infra/locality_model.dart';
+import 'package:nurse/shared/repositories/database/infra/database_establishment_repository.dart';
 import 'package:nurse/shared/repositories/database/infra/database_locality_repository.dart';
 import 'package:nurse/shared/repositories/infra/establishment_repository.dart';
 import 'package:nurse/shared/repositories/infra/locality_repository.dart';
 
 class EstablishmentFormController extends FormController {
+  final EstablishmentRepository _repository;
   final LocalityRepository _localityRepository;
 
-  final _localities = List<Locality>.empty(growable: true);
+  final _localityCities = List<Locality>.empty(growable: true);
+  List<Locality> get localityCities => _localityCities;
 
   TextEditingController cnes = TextEditingController();
   TextEditingController name = TextEditingController();
@@ -17,20 +21,64 @@ class EstablishmentFormController extends FormController {
   EstablishmentFormController([
     EstablishmentRepository? establishmentRepository,
     LocalityRepository? localityRepository,
-  ]) : _localityRepository = localityRepository ?? DatabaseLocalityRepository();
+  ])  : _repository =
+            establishmentRepository ?? DatabaseEstablishmentRepository(),
+        _localityRepository =
+            localityRepository ?? DatabaseLocalityRepository();
 
-  void getLocalities(String state, String city) async {
-    _localities.addAll(
-      await _localityRepository.getLocalitiesByStateAndCity(state, city),
-    );
+  Future<List<Locality>> getCitiesFromLocalities() async {
+    final localities = await _localityRepository.getLocalities();
+    final oneLocalityByCity = List<Locality>.empty(growable: true);
 
-    notifyListeners();
+    localities.forEach((locality) {
+      final isCityAlreadyAdded = oneLocalityByCity.any((city) {
+        return city.city == locality.city;
+      });
+
+      if (isCityAlreadyAdded) return;
+
+      oneLocalityByCity.add(locality);
+    });
+
+    _localityCities.addAll(oneLocalityByCity);
+
+    return _localityCities;
   }
 
+  @override
   void clearAllInfo() {
     cnes.clear();
     name.clear();
     locality = null;
+
+    notifyListeners();
+  }
+
+  Future<bool> saveInfo() async {
+    submitForm();
+    final allFieldsValid = super.formKey.currentState!.validate();
+
+    if (allFieldsValid) {
+      try {
+        final id = await _repository.createEstablishment(Establishment(
+          cnes: cnes.text,
+          name: name.text,
+          locality: locality!,
+        ));
+
+        if (id != 0) {
+          clearAllInfo();
+          return true;
+        } else {
+          return false;
+        }
+      } catch (error) {
+        print(error);
+        return false;
+      }
+    }
+
+    return false;
   }
 
   @override
